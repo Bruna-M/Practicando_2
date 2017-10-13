1683d76737639aa9af3fb83a5be4302c2f61fcf7
christianempire <christianempire@hotmail.com>
2017-10-12 23:13:43 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hola chicos
+
+- Creen un archivo con su codigo y resuelvan ahi las preguntas
+
+### Atte: 5tana
---
+++ base.py
@@ -0,0 +1,7 @@
+class MiClase():
+	self.Atributo1
+	self.Atributo2
+
+	def __init__():
+		self.Atributo1 = "Valor1"
+		self.Atributo2 = "Valor2"
